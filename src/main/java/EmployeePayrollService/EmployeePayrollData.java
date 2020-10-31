43f4413b589e0fa3f3bf1f164c3e2dcb0ee1bbae
shubham <shubhamkumar10516@gmail.com>
2020-10-31 05:30:14 +0530

    [Shubham]REFACTORED: Constructor refactored to have start date as entry

--- src/main/java/EmployeePayrollService/EmployeePayrollData.java
+++ src/main/java/EmployeePayrollService/EmployeePayrollData.java
@@ -1,14 +1,24 @@
 package EmployeePayrollService;
 
+import java.time.LocalDate;
+
 public class EmployeePayrollData {
 
 	private int id;
 	private String name;
 	private double salary;
+	private LocalDate startDate; 
 	public EmployeePayrollData(int id, String name, double salary) {
 		this.id = id;
 		this.name = name;
 		this.salary = salary;
+	}
+	public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+        this(id, name, salary);
+		this.startDate = startDate;
+	}
+	public String toString() {
+	     return (id+":"+ name+ ":"+ salary+" : "+startDate);
 	}
 	public String display() {
 	     return (id+":"+ name+ ":"+ salary);
